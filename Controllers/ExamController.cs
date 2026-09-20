0086c448264f1934fa6cd6dd5b640e53b1e8eccb
WspJon <realesjonnidel@gmail.com>
2026-09-21 04:54:29 +0800

    Setup ExamController and Index view

---
+++ Controllers/ExamController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_Reales_Jonnidel.Controllers
+{
+    public class ExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
