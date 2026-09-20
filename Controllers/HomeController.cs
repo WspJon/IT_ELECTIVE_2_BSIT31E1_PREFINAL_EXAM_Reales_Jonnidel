11fadac83227d980ef16141615aa91c28612a93f
WspJon <realesjonnidel@gmail.com>
2026-09-21 04:53:22 +0800

    Initial commit: MVC template

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_Reales_Jonnidel.Models;
+
+namespace IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_Reales_Jonnidel.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
